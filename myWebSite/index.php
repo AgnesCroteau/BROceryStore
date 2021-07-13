eb17559f7d7e700f9c04f49f847295c8571933a7
Agnes Croteau <a_roteau@live.concordia.ca>
2021-07-13 13:25:02 -0400

    first commit

---
+++ myWebSite/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Hello, World! | Foo</title>
+  </head>
+  <body>
+    <h1>Hello, World!</h1>
+    <p><strong>Welcome to my website!</strong>
+    First, let me introduce myself.  My name is ALICE, and I'm dating BOB. </p>
+    <h2>We met at Chapters and it's been heaven on earth ever since.</h2>
+    <p>I knew something was up when I saw his pretty face with his huge, glossy eyes.  However, it's his body odour that first got me hooked.</p>
+    <p>'' Do you come here often? '' he said.</p>
+    <p>'' Always! '' I answered, trying to look honest.  It was the first time I'd actually come accross the Computer section. I was completely lost.</p>
+    <p> Later on, I realized loving him was in my <abbr title="Deoxyribonucleic acid">DNA</abbr>. </p>
+    <p><abbr title="Mister">Mr.</abbr> BOB, boy were you hot.</p>
+  </body>
+</html>
---
+++ secondWebsite/index.php
@@ -0,0 +1,25 @@
+<body>
+    <h1>Hello World</h1>
+    <p> Welcome to my website.  Here, I will be 100% dedicated to learning Bulgarian, my mother's tongue, and helping you doing the same along the way.</p>
+
+    <h2> Are you ready??? </h2>
+    <p>  Alright, we'll get going. However, firstly I'd like to thank my <b>mom</b>, for giving me this challenge today (better late than never!). 
+    I'm also thanking <b>Big Sis</b>, who suggested the great book that is <i>Le Bulgare sans peine</i>, from the <i>Assimil collection</i>. 
+    Thanks to the Assimil team, huge props to all of you guys! As for you, the <b>readers</b>, if you go get this book (at BAnQ or as an online purchase), 
+     make sure you get the big, complete version and not the puny one... Take the one with the audio CDs included, you'll thank me later! 
+     (As I thanked Big Sis just a tiny bit earlier today ;)
+
+    Last but not least, many thanks to any Bulgarian in my surroundings who are willing to help me, with their patience and knowledge of the language.
+    <b>Dre D.</b>, I'm looking your way! As for you, <b>Timousse</b>, your <i>"pet pile"</i> makes me laugh everytime... you deserve your name in my
+     <b>THANK YOU</b> 
+    list because without you, this website would have never existed, ever, and that's the truth! 
+    <br /><br />
+    <i>xxxx
+    AMC</i> <p>
+    <br /><hr /><br />
+    <h3> Step #1 </h3>
+   <p> If you're like me, you already know how to say <strong>"Dober Den" </strong> (bonjour, hi) <span lang="bg">Добър ден</span> and you have at least a very vague idea of the alphabet.
+    (If not, take a look at <a href="http://www.roumiana.com/cours_alphabet.htm">this</a> )</p>
+
+
+</body>
